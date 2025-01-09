5f253fb4dce451dbf1ecb5e11046950dedda2277
Yash Patel <yash.patel@unifyapps.com>
2025-01-09 17:46:11 +0530

    Added Redditservice Layer to map output fields with Response class

---
+++ src/main/java/org/dev/servicelayer/RedditService.java
@@ -0,0 +1,19 @@
+package org.dev.servicelayer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.dev.Entity.RedditPostresponse;
+
+import java.io.IOException;
+
+@ApplicationScoped
+public class RedditService{
+    public RedditPostresponse parseRedditResponse(String jsonResponse) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(jsonResponse, RedditPostresponse.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse Reddit API response", e);
+        }
+    }
+}
