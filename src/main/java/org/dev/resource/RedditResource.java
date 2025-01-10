5c7ac97d8d80940edb3fc57089a61192ce2a4390
Yash Patel <yash.patel@unifyapps.com>
2025-01-10 16:21:49 +0530

    added aync and kafkaproducer function call

--- src/main/java/org/dev/resource/RedditResource.java
+++ src/main/java/org/dev/resource/RedditResource.java
@@ -6,13 +6,20 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.dev.Client.RedditClient;
 import org.dev.Entity.RedditPostresponse;
+import org.dev.Entity.RedditResponse;
+import org.dev.Kafka.KafkaPostProducer;
 import org.dev.servicelayer.RedditService;
 import org.dev.tokengenerator.Tokengenerator;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 @Path("/author-posts-api")
 public class RedditResource {
 
+    @Inject
+    KafkaPostProducer kafkaPostProducer;
     @Inject
             @RestClient
     RedditClient redditClient;
@@ -23,11 +30,16 @@ public class RedditResource {
     @GET
     @Path("/{username}")
     @Produces(MediaType.APPLICATION_JSON)
-    public RedditPostresponse getPostbyAuthorName(@PathParam("username") String username, @QueryParam("limit") int limit)
+    public CompletableFuture<RedditPostresponse> getPostbyAuthorName(@PathParam("username") String username, @QueryParam("limit") int limit)
     {
-        String Token="Bearer " +tokenfetch.getToken();
-        String res= redditClient.getPostsByUser(username,limit,Token);
-        return redditService.parseRedditResponse(res);
+        return CompletableFuture.supplyAsync(() -> {
+            String token = "Bearer " + tokenfetch.getToken();
+            String res = redditClient.getPostsByUser(username, limit, token);
+            RedditPostresponse response = redditService.parseRedditResponse(res);
+            kafkaPostProducer.sendPostsToKafka(response);
+
+            return response;
+        });
     }
 
 }
