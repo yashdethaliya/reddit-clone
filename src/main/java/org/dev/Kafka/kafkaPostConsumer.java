77c17c4bc64d114dff1ce69e99ca5509e4b3b620
Yash Patel <yash.patel@unifyapps.com>
2025-01-11 15:33:46 +0530

    Added opensearchindexservice function call to index data

--- src/main/java/org/dev/Kafka/kafkaPostConsumer.java
+++ src/main/java/org/dev/Kafka/kafkaPostConsumer.java
@@ -3,20 +3,21 @@ package org.dev.Kafka;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.inject.Inject;
 import org.dev.Entity.RedditResponse;
+import org.dev.OpenSeacrh.OpenSearchIndexservice;
 import org.dev.mongo.MongoDBService;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 public class kafkaPostConsumer {
     @Inject
+    OpenSearchIndexservice openSearchIndexservice;
+    @Inject
     private MongoDBService mongoDBService;
-
     @Incoming("reddit-posts-consumer")
     public void consume(String message) {
         try {
             RedditResponse postData = new ObjectMapper().readValue(message, RedditResponse.class);
             mongoDBService.savePostData(postData);
-
-            System.out.println("Consumed message: " + message);
+            openSearchIndexservice.indexDataIntoOpenSearch(message);
         } catch (Exception e) {
             System.err.println("Error consuming message: " + e.getMessage());
         }
