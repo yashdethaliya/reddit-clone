6f677c09c6b77da1c6f41e61341eadae7cd725d0
Yash Patel <yash.patel@unifyapps.com>
2025-01-14 16:38:33 +0530

    Added two method to fetch trending user data and to fetch existing user data

--- src/main/java/org/dev/mongo/MongoDBService.java
+++ src/main/java/org/dev/mongo/MongoDBService.java
@@ -5,13 +5,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.Document;
 import org.dev.Entity.RedditResponse;
+import org.dev.Entity.TrendingUser;
+
+import java.util.*;
 
 @ApplicationScoped
 public class MongoDBService {
-    private static final MongoDatabase database=MongoClients.create("mongodb://localhost:27017").getDatabase("reddit");;
+    private static final MongoDatabase database=MongoClients.create("mongodb://localhost:27017").getDatabase("reddit");
 
 
     public void savePostData(RedditResponse postData) throws JsonProcessingException {
@@ -21,5 +27,31 @@ public class MongoDBService {
         collection.insertOne(doc);
     }
 
+    public List<TrendingUser> finetredingusers()
+    {
+        MongoCollection<Document> collection=database.getCollection("posts");
+        List<Document> aggregationResult = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.group("$author", Accumulators.sum("postCount", 1)),
+                        Aggregates.sort(Sorts.descending("postCount"))
+                )
+        ).into(new ArrayList<>());
+
+        List<TrendingUser> userPostCounts = new ArrayList<>();
+        for (Document doc : aggregationResult) {
+            String name = doc.getString("_id");
+            int count = doc.getInteger("postCount");
+            userPostCounts.add(new TrendingUser(name, count));
+        }
+        return userPostCounts;
+
+    }
+    public Set<String> getexistingURLs(String username)
+    {
+        MongoCollection<Document> collection = database.getCollection("posts");
+        return collection.find(new Document("author", username))
+                .map(doc -> doc.getString("url"))
+                .into(new HashSet<>());
+    }
 
 }
